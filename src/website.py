1bd3c2db76bb0a8d29fda749c797997a9b9f0d93
ilcheese2 <56899583+ilcheese2@users.noreply.github.com>
2023-09-01 19:45:18 -0700

    Added basic decoding endpoint

--- src/website.py
+++ src/website.py
@@ -4,7 +4,7 @@ from io import BytesIO
 from flask import Flask, render_template, request
 from PIL import Image
 
-from processing import generate_alphabet, write_message
+from processing import generate_alphabet, read_message, write_message
 
 app = Flask(__name__)
 
@@ -18,18 +18,20 @@ def index():  # noqa: D103
 def encode():  # noqa: D103
     if request.method == "GET":
         return render_template("encode.html")
-    else:
-        io = BytesIO()
-        image = Image.open(request.files.get('image').stream)
-        write_message(image, request.form["message"], request.form["key"])[0].save(io, format="PNG")
-        io.seek(0)
-        return render_template("display.html",
-                               src=f"data:image/png;base64,{base64.b64encode(io.read()).decode('utf-8')}")
+    io = BytesIO()
+    image = Image.open(request.files.get('image').stream)
+    write_message(image, request.form["message"], request.form["key"])[0].save(io, format="PNG")
+    io.seek(0)
+    return render_template("display.html",
+                           src=f"data:image/png;base64,{base64.b64encode(io.read()).decode('utf-8')}")
 
 
-@app.route("/decode")
+@app.route("/decode", methods=["GET", "POST"])
 def decode():  # noqa: D103
-    return render_template("decode.html")
+    if request.method == "GET":
+        return render_template("decode.html")
+    image = Image.open(request.files.get('image').stream)
+    return read_message(image, request.form["key"])
 
 
 @app.route("/generate")
