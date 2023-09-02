8edc69c6c69d2fe3f4757bb7c5ef28cde736163c
ilcheese2 <56899583+ilcheese2@users.noreply.github.com>
2023-09-01 19:32:42 -0700

    Added basic encoding endpoint

--- src/website.py
+++ src/website.py
@@ -1,13 +1,35 @@
-from flask import Flask, request
+import base64
+from io import BytesIO
 
-from processing import generate_alphabet
+from flask import Flask, render_template, request
+from PIL import Image
+
+from processing import generate_alphabet, write_message
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():  # noqa: D103
-    return "<h1>Placeholder, see <a href=/generate>/generate</a> for integration with image_processing module</h1>"
+    return render_template("index.html")
+
+
+@app.route("/encode", methods=["GET", "POST"])
+def encode():  # noqa: D103
+    if request.method == "GET":
+        return render_template("encode.html")
+    else:
+        io = BytesIO()
+        image = Image.open(request.files.get('image').stream)
+        write_message(image, request.form["message"], request.form["key"])[0].save(io, format="PNG")
+        io.seek(0)
+        return render_template("display.html",
+                               src=f"data:image/png;base64,{base64.b64encode(io.read()).decode('utf-8')}")
+
+
+@app.route("/decode")
+def decode():  # noqa: D103
+    return render_template("decode.html")
 
 
 @app.route("/generate")
