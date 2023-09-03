fc2c8ff0b47df3148bbb8d1e6fd4ec131d2592e5
Kronifer <44979306+Kronifer@users.noreply.github.com>
2023-09-02 19:49:01 -0500

    feat: refactored backend functions and added RGB read_message, write_message, and generate_alphabet

--- src/website.py
+++ src/website.py
@@ -5,7 +5,7 @@ from io import BytesIO
 from flask import Flask, render_template, request
 from PIL import Image
 
-from .processing import read_message, write_message
+from .processing import read_message_a, write_message_a
 
 template_dir = os.path.abspath("src/assets")
 app = Flask(__name__, template_folder=template_dir)
@@ -23,7 +23,7 @@ def encode():  # noqa: D103
         return render_template("encode.html")
     io = BytesIO()
     image = Image.open(request.files.get("image").stream)
-    image, status = write_message(image, request.form["message"].lower(), request.form["key"])
+    image, status = write_message_a(image, request.form["message"].lower(), request.form["key"])
     if not status:
         return (
             "ERROR: Please make sure your message is not too long, and that you don't have any unicode characters in your message (no emojis).",  # noqa: E501
@@ -40,4 +40,4 @@ def decode():  # noqa: D103
     if request.method == "GET":
         return render_template("decode.html")
     image = Image.open(request.files.get("image").stream)
-    return read_message(image, request.form["key"]), {"Content-Type": "text/plain"}
+    return read_message_a(image, request.form["key"]), {"Content-Type": "text/plain"}
